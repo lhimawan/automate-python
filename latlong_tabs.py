76915540ce87e965b28d99108f178907d8c59b67
Linden <himawanlinden@gmail.com>
2020-07-19 10:38:22 +0700

    added regex to fix GMaps input comma bug

--- latlong_tabs.py
+++ latlong_tabs.py
@@ -1,16 +1,18 @@
-import webbrowser
+import re, webbrowser
 
 def search():
 #Takes long lat input & stores them into variables
-    latlong = input('Please enter the lat & long:').split()
+    latlongRegex = re.compile(r'[\d.-]+')
+    latlong = latlongRegex.findall(input('Please enter the lat & long:'))
     zoom = '17' #set map zoom here TODO: zoom setting?
 
 #Open GMaps, OSM, HERE, and Mapy.cz with specified coordinates
     webbrowser.open('https://www.google.com/maps/@%s,%s,%sz' % (latlong[0], latlong[1], zoom))
     webbrowser.open('https://www.openstreetmap.org/#map=%s/%s/%s' % (zoom, latlong[0], latlong[1]))
+    #source rarely used, commented out for now
+    #webbrowser.open('https://wego.here.com/?map=%s,%s,%s,normal' % (latlong[0], latlong[1], zoom)) 
     #swapped coordinates for the source below:
     webbrowser.open('https://en.mapy.cz/zakladni?x=%s&y=%s&z=%s' % (latlong[1], latlong[0], zoom))
-    #webbrowser.open('https://wego.here.com/?map=%s,%s,%s,normal' % (latlong[0], latlong[1], zoom)) #source rarely used, commented out for now
     
 #Flow control: loop back to start of program
 run = True
